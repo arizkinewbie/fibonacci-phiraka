66da459116c4d82c2171170b57ed7fba63d19447
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-03-13 11:11:15 +0700

    change handler

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
       var rows = document.querySelector('input[name="rows"]').value;
       var columns = document.querySelector('input[name="columns"]').value;
       var baseURL = document.querySelector('meta[name="base-url"]').getAttribute("content");
+      if (columns <= 0 || rows <= 0) {
+        console.error("Server responded with an error.");
+        alert("kolom dan baris harus lebih dari angka 0.");
+      }
       // Perform the AJAX request
       var xhr = new XMLHttpRequest();
       xhr.open("POST", baseURL + "generate", true);
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xhr.onload = function () {
         // On success, update the page content
-        if (this.status >= 200 && this.status < 400) {
-          if (columns <= 0 || rows <= 0) {
-            console.error("Server responded with an error.");
-            alert("kolom dan baris harus lebih dari angka 0.");
-          }
+        if (this.status >= 200 && this.status < 400 ) {
           document.getElementById("fibonacci-table").innerHTML = this.response;
         } else {
           console.error("Server responded with an error.");
